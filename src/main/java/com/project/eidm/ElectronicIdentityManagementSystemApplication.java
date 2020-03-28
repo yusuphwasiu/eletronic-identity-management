75d6e1b0f3a83bf0e32f3ce6987ddd9bf13e1843
Yusuf Wasiu <yusuphwasiu@gmail.com>
2020-03-28 23:08:04 +0100

    project innitialization

---
+++ src/main/java/com/project/eidm/ElectronicIdentityManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.eidm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicIdentityManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicIdentityManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/eidm/ElectronicIdentityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eidm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicIdentityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
