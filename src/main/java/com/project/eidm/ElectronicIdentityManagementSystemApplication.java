ac28c475699ba5e88a8abc7b5ac4b6f1bd00e72a
Yusuf Wasiu <yusuphwasiu@gmail.com>
2020-03-29 00:33:41 +0100

    project innitialization complete

--- src/main/java/com/project/eidm/ElectronicIdentityManagementSystemApplication.java
+++ src/main/java/com/project/eidm/ElectronicIdentityManagementSystemApplication.java
@@ -1,8 +1,12 @@
 package com.project.eidm;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ElectronicIdentityManagementSystemApplication {
 
